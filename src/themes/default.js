b8a5322505f9cbcc7a6be2edef12f347c8bddd89
Ankur Gurjar <76687403+8ankur8@users.noreply.github.com>
2023-01-09 12:29:29 +0530

    Update default.js

--- src/themes/default.js
+++ src/themes/default.js
@@ -7,7 +7,7 @@ export default {
   // Colors for layout
   colors: {
     primary1: "hsl(204,23.8%,95.9%)",
-    background1: "rgba(255, 205, 200, 0.75)",
+    background1: "rgba(255, 205, 200, 0.5)",
     button: "hsl(205.1,100%,36.1%)",
     background2: "hsl(232.7,27.3%,23.7%)",
   },
